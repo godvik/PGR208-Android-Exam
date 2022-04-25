4701461c379f446945fe4cc0609d8db212c3a769
Alexandra Baker <baker.alexandra88@gmail.com>
2022-04-25 23:02:41 +0200

    Cant access gallery?

--- app/src/main/java/no/kristiania/pgr208/MainActivity.kt
+++ app/src/main/java/no/kristiania/pgr208/MainActivity.kt
@@ -1,6 +1,7 @@
 package no.kristiania.pgr208
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -24,8 +25,8 @@ import no.kristiania.pgr208.Constants.baseUrl
 import no.kristiania.pgr208.utils.BitmapHelper.bitmapToFileUri
 import no.kristiania.pgr208.utils.BitmapHelper.getBytes
 import com.theartofdev.edmodo.cropper.CropImage
-import com.edmodo.cropper.CropImageView
-import android.graphics.drawable.BitmapDrawable
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
 
 import java.io.*
 import java.util.*
@@ -33,14 +34,24 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var imgView: ImageView
     private var imageFile: File? = null
     var uploadedImageURL: String? = null
     private var bitmap: Bitmap? = null
     private lateinit var db: DatabaseHandler
-    public lateinit var image: CropImageView
-    public var imageUri: String? = null
 
+    private val cropActivityResultContract = object : ActivityResultContract<Any?, Uri>() {
+        override fun createIntent(context: Context, input: Any?): Intent {
+            return CropImage.activity()
+                .setAspectRatio(16, 9)
+                .getIntent(this@MainActivity)
+        }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): Uri {
+            return CropImage.getActivityResult(intent)?.uri!!
+        }
+    }
+
+    private lateinit var cropActivityResultLauncher: ActivityResultLauncher<Any?>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +60,13 @@ class MainActivity : AppCompatActivity() {
         AndroidNetworking.initialize(applicationContext)
         AndroidNetworking.enableLogging()
         db = DatabaseHandler(this)
-        imgView = findViewById(R.id.iv_userImage)
+        val imgView = findViewById<ImageView>(R.id.iv_userImage)
+
+        cropActivityResultLauncher = registerForActivityResult(cropActivityResultContract) {
+            it?.let { uri ->
+                imgView.setImageURI(uri)
+            }
+        }
 
 
 //        Select image from gallery, convert it to bitmap and set the imageView to the bitmap
@@ -64,14 +81,9 @@ class MainActivity : AppCompatActivity() {
             }
 
 //        Launch the galleryPicker
-        val selectImageBtn: Button = findViewById(R.id.selectImageBtn)
+        val selectImageBtn = findViewById<Button>(R.id.selectImageBtn)
         selectImageBtn.setOnClickListener {
-            var i = Intent()
-
-            i.action = Intent.ACTION_GET_CONTENT
-            i.type = "image/*"
-
-            galleryLauncher.launch(i.toString())
+            cropActivityResultLauncher.launch("*/*")
         }
 
 //        Sends the uploadedImageURL to the next activity to be used for GET requests
